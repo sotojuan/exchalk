3a67fd1a3c9a17893c28dd168c81c378f0d2b9af
Juan Soto <juan@juansoto.me>
2016-09-28 12:38:13 -0400

    1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExChalk.Mixfile do
   def project do
     [
       app: :exchalk,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
