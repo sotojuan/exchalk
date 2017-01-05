8e4394351301bb3c80b61fe6164f067547b1812b
Juan Soto <juan@juansoto.me>
2017-01-05 08:10:19 -0500

    Add parentheses to make Elixir 1.4 happy

--- mix.exs
+++ mix.exs
@@ -8,9 +8,9 @@ defmodule ExChalk.Mixfile do
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      description: description,
-      package: package,
-      deps: deps
+      description: description(),
+      package: package(),
+      deps: deps()
     ]
   end
 
